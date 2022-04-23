f01a384b9fe62ebbbdb0855eb3e351c4b49cdd06
Miguel Magueijo <miguelmagueijo@hotmail.com>
2022-04-23 01:29:47 +0100

    Add tailwind, first interface of a match

---
+++ src/routes/summoner.svelte
@@ -0,0 +1,218 @@
+<script>
+    import "../app.css";
+</script>
+
+<div class="w-4/5 mx-auto bg-red-700 grid grid-cols-12 py-4">
+    <div class="col-span-4 bg-blue-500">
+        info
+    </div>
+    <div class="col-span-8">
+        
+        
+        <div class="bg-red loss-glow m-4 grid grid-cols-7 rounded" style="max-height: 250px;">
+            <div class="flex space-x-2">
+                <div class="overflow-hidden rounded-l" style="max-width: 85px;">
+                    <img src="/img/Rell_0.jpg" class="h-full scale-110" alt="ChampionImage">
+                </div>
+                <div class="flex flex-col justify-between items-center py-2">
+                    <img src="/img/SummonerFlash.png" class="rounded" width="25px" alt="Spell 1">
+                    <img src="/img/SummonerDot.png" class="rounded" width="25px" alt="Spell 2">
+                    <img src="/img/Conqueror.png" class="rounded-xl gray-img-bg" width="25px" alt="Spell 1">
+                    <img src="/img/Resolve.png" class="rounded-xl gray-img-bg" width="25px" alt="Spell 1">
+                </div>
+            </div>
+            <div class="text-white my-auto">
+                <p class="font-bold text-4xl">DEFEAT</p>
+                <p class="opacity-80">NORMAL DRAFT</p>
+                <p class="opacity-80">18:23</p>
+                <p class="opacity-80">2 DAYS AGO</p>
+            </div>
+            <div class="text-center my-auto">
+                <p class="text-4xl">0/6/3</p>
+                <p class="text-xl opacity-80">0.5 KDA</p>
+                <p class="text-xl opacity-80">P/Kill 38%</p>
+            </div>
+            <div class="my-auto">
+                <p class="opacity-80">Rell</p>
+                <p class="opacity-80">LVL 8</p>
+                <p class="opacity-80">28CS (1.5)</p>
+                <p class="opacity-80">21 VISION</p>
+            </div>
+            <div class="grid grid-cols-4 grid-rows-2 gap-2 my-auto">
+                <div>
+                    <img src="/img/item1.png" class="rounded" alt="Item 1">
+                </div>
+                <div>
+                    <img src="/img/item2.png" class="rounded" alt="Item 1">
+                </div>
+                <div>
+                    <img src="/img/item3.png" class="rounded" alt="Item 1">
+                </div>
+                <div class="row-span-2">
+                    <img src="/img/item7.png" class="min-h-full object-none rounded" alt="Item 1">
+                </div>
+                <div>
+                    <img src="/img/item4.png" class="rounded" alt="Item 1">
+                </div>
+                <div>
+                    <img src="/img/item5.png" class="rounded" alt="Item 1">
+                </div>
+                <div>
+                    <img src="/img/item6.png" class="rounded" alt="Item 1">
+                </div>
+            </div>
+            <div class="col-span-2 space-y-1 my-auto text-center">
+                <div class="space-x-1">
+                    <span class="text-sm">Blue Player 1</span>
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl" alt="Cham Lane" width="25px">
+                    <img src="/vector/TopLane.svg" class="inline-block" alt="Top Lane" width="25px">
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl flip-horizontal" alt="Top Lane" width="25px">
+                    <span class="text-sm">Red Player 1</span>
+                </div>
+                <div class="space-x-1">
+                    <span class="text-sm">Blue Player 2</span>
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl" alt="Cham Lane" width="25px">
+                    <img src="/vector/Jungle.svg" class="inline-block" alt="Jungle" width="25px">
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl flip-horizontal" alt="Top Lane" width="25px">
+                    <span class="text-sm">Red Player 2</span>
+                </div>
+                <div class="space-x-1">
+                    <span class="text-sm">Blue Player 3</span>
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl" alt="Cham Lane" width="25px">
+                    <img src="/vector/MidLane.svg" class="inline-block" alt="Top Lane" width="25px">
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl flip-horizontal" alt="Top Lane" width="25px">
+                    <span class="text-sm">Red Player 3</span>
+                </div>
+                <div class="space-x-1">
+                    <span class="text-sm">Blue Player 4</span>
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl" alt="Cham Lane" width="25px">
+                    <img src="/vector/BotLane.svg" class="inline-block" alt="Adc" width="25px">
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl flip-horizontal" alt="Top Lane" width="25px">
+                    <span class="text-sm">Red Player 4</span>
+                </div>
+                <div class="space-x-1">
+                    <span class="text-sm">Blue Player 5</span>
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl" alt="Cham Lane" width="25px">
+                    <img src="/vector/Support.svg" class="inline-block" alt="Support" width="25px">
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl flip-horizontal" alt="Top Lane" width="25px">
+                    <span class="text-sm">Red Player 5</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="bg-blue win-glow m-4 grid grid-cols-7 rounded" style="max-height: 250px;">
+            <div class="flex space-x-2">
+                <div class="overflow-hidden rounded-l" style="max-width: 85px;">
+                    <img src="/img/Rell_0.jpg" class="h-full scale-110" alt="ChampionImage">
+                </div>
+                <div class="flex flex-col justify-between items-center py-2">
+                    <img src="/img/SummonerFlash.png" class="rounded" width="25px" alt="Spell 1">
+                    <img src="/img/SummonerDot.png" class="rounded" width="25px" alt="Spell 2">
+                    <img src="/img/Conqueror.png" class="rounded-xl gray-img-bg" width="25px" alt="Spell 1">
+                    <img src="/img/Resolve.png" class="rounded-xl gray-img-bg" width="25px" alt="Spell 1">
+                </div>
+            </div>
+            <div class="text-white my-auto">
+                <p class="font-bold text-4xl">VICTORY</p>
+                <p class="opacity-80">NORMAL DRAFT</p>
+                <p class="opacity-80">18:23</p>
+                <p class="opacity-80">2 DAYS AGO</p>
+            </div>
+            <div class="text-center my-auto">
+                <p class="text-4xl">7/8/28</p>
+                <p class="text-xl opacity-80">5.83 KDA</p>
+                <p class="text-xl opacity-80">P/Kill 76%</p>
+            </div>
+            <div class="my-auto">
+                <p class="opacity-80">Rell</p>
+                <p class="opacity-80">LVL 8</p>
+                <p class="opacity-80">28CS (1.5)</p>
+                <p class="opacity-80">63 VISION</p>
+            </div>
+            <div class="grid grid-cols-4 grid-rows-2 gap-2 my-auto">
+                <div>
+                    <img src="/img/item1.png" class="rounded" alt="Item 1">
+                </div>
+                <div>
+                    <img src="/img/item2.png" class="rounded" alt="Item 1">
+                </div>
+                <div>
+                    <img src="/img/item3.png" class="rounded" alt="Item 1">
+                </div>
+                <div class="row-span-2">
+                    <img src="/img/item7.png" class="min-h-full object-none rounded" alt="Item 1">
+                </div>
+                <div>
+                    <img src="/img/item4.png" class="rounded" alt="Item 1">
+                </div>
+                <div>
+                    <img src="/img/item5.png" class="rounded" alt="Item 1">
+                </div>
+                <div>
+                    <img src="/img/item6.png" class="rounded" alt="Item 1">
+                </div>
+            </div>
+            <div class="col-span-2 my-auto text-center">
+                <div class="space-x-1">
+                    <span class="text-sm">Blue Player 1</span>
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl" alt="Cham Lane" width="25px">
+                    <img src="/vector/TopLane.svg" class="inline-block" alt="Top Lane" width="25px">
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl flip-horizontal" alt="Top Lane" width="25px">
+                    <span class="text-sm">Red Player 1</span>
+                </div>
+                <div class="space-x-1">
+                    <span class="text-sm">Blue Player 2</span>
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl" alt="Cham Lane" width="25px">
+                    <img src="/vector/Jungle.svg" class="inline-block" alt="Jungle" width="25px">
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl flip-horizontal" alt="Top Lane" width="25px">
+                    <span class="text-sm">Red Player 2</span>
+                </div>
+                <div class="space-x-1">
+                    <span class="text-sm">Blue Player 3</span>
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl" alt="Cham Lane" width="25px">
+                    <img src="/vector/MidLane.svg" class="inline-block" alt="Top Lane" width="25px">
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl flip-horizontal" alt="Top Lane" width="25px">
+                    <span class="text-sm">Red Player 3</span>
+                </div>
+                <div class="space-x-1">
+                    <span class="text-sm">Blue Player 4</span>
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl" alt="Cham Lane" width="25px">
+                    <img src="/vector/BotLane.svg" class="inline-block" alt="Adc" width="25px">
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl flip-horizontal" alt="Top Lane" width="25px">
+                    <span class="text-sm">Red Player 4</span>
+                </div>
+                <div class="space-x-1">
+                    <span class="text-sm">Blue Player 5</span>
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl" alt="Cham Lane" width="25px">
+                    <img src="/vector/Support.svg" class="inline-block" alt="Support" width="25px">
+                    <img src="/img/Rell_0_tile.jpg" class="inline-block rounded-xl flip-horizontal" alt="Top Lane" width="25px">
+                    <span class="text-sm">Red Player 5</span>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<style>
+    .flip-horizontal {
+        transform: scale(-1, 1);
+    }
+
+    .loss-glow {
+        -webkit-box-shadow:0px 0px 50px 11px rgba(102,13,13,0.35);
+        -moz-box-shadow: 0px 0px 50px 11px rgba(102,13,13,0.35);
+        box-shadow: 0px 0px 50px 11px rgba(102,13,13,0.35);
+    }
+
+    .win-glow {
+        -webkit-box-shadow:0px 0px 50px 11px rgba(7,50,86,0.35);
+        -moz-box-shadow: 0px 0px 50px 11px rgba(7,50,86,0.35);
+        box-shadow: 0px 0px 50px 11px rgba(7,50,86,0.35);
+    }
+
+    .gray-img-bg {
+        background-color: rgba(0, 0, 0, 0.5);
+        padding: 3px;
+    }
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "red": "#530404",
+        "blue": "#073356",
+        "dark": "#111111",
+      }
+    }
+  },
+  plugins: []
+};
